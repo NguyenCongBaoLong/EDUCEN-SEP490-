7afc817d556a05f584042a0050c67490d0bdd617
Nguyen Tuan Anh HE171413 <anhnthe171413@fpt.edu.vn>
2026-02-28 22:30:31 +0700

    login,authen,author

--- BE/EducenAPI/Program.cs
+++ BE/EducenAPI/Program.cs
@@ -1,11 +1,48 @@
+using EducenAPI.Services;
+using EducenAPI.Services.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using EducenAPI.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<EducenV2Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDatabase")));
+
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
 builder.Services.AddSwaggerGen();
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -17,9 +54,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
---
+++ BE/EducenAPI/Services/Interface/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using EducenAPI.DTOs;
+
+namespace EducenAPI.Services.Interface
+{
+    public interface IAuthService
+    {
+        Task<string> Login(LoginDto dto);
+        Task Register(RegisterDto dto);
+    }
+
+}
