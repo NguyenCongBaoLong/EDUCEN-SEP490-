6aa67689dc90fe896403fedf367262a2537ac891
NguyenTuanAnhHE171413 <tuan2003ta@gmail.com>
2026-03-16 08:20:59 +0700

    feat(ResourceFile): add new entity

---
+++ BE/EducenAPI/Models/ResourceFile.cs
@@ -0,0 +1,25 @@
+﻿namespace EducenAPI.Models
+{
+    public class ResourceFile
+    {
+        public int Id { get; set; }
+
+        public string FileName { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string FilePath { get; set; } = string.Empty;
+
+        public string Extension { get; set; } = string.Empty;
+
+        public long? FileSize { get; set; }
+
+        public int? LessonMaterialId { get; set; }
+
+        public int? AssignmentId { get; set; }
+
+        public Assignment? Assignment { get; set; }
+
+        public LessonMaterial? LessonMaterial { get; set; }
+    }
+}
